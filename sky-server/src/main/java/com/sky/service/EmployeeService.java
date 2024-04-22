d05888455ed0de86c4574ad5324f4f178c3c1ebf
戴佳成 <2437884858@qq.com>
2024-04-22 10:54:12 +0800

    sky1启用禁用员工账号

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -23,4 +23,10 @@ public interface EmployeeService {
     void save(EmployeeDTO employeeDTO);
 
     PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
+
+    /*
+    * 启用禁用员工账号
+    *
+    * */
+    void startOrStop(Integer status, Long id);
 }
